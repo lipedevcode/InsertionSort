ec1c38b01a003d15da980fa0d082b970f80f904b
Luis Felipe Ferreira de Castro <147777014+lipedevcode@users.noreply.github.com>
2023-11-16 19:36:54 -0300

    Add files via upload
    ADT insert-sort-algorithm

---
+++ insertionSort.java
@@ -0,0 +1,24 @@
+
+public class insertionSort {
+    public void sort(int[] arr, int n) {
+        int[] newArr = new int[n];
+        newArr[0] = arr[0];
+        boolean swapped;
+        int temp;
+        for (int i = 1; i < n; i++) {
+            swapped = false;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] < newArr[j]) {
+                    temp = newArr[j];
+                    newArr[j] = arr[i];
+                    newArr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                newArr[i] = arr[i];
+        }
+        for (int i = 0; i < n; i++)
+            arr[i] = newArr[i];
+    }
+}
