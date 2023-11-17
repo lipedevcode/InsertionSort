a661143b3896d2a8b5f7123f1712feff778c3d6a
Luis Felipe Ferreira de Castro <147777014+lipedevcode@users.noreply.github.com>
2023-11-17 09:42:34 -0300

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -1,25 +1,21 @@
-public class selectionSort {
-    public void sort(int[] arr, int n) {
-        int minIndex = 0;
-        boolean swapped;
-        for (int i = 0; i < n; i++) {
-            minIndex = i;
-            swapped = false;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                    swapped = true;
-                }
-            }
-            if (swapped) {
-                swap(arr, minIndex, i);
+public void sort(int[] arr, int n) {
+    boolean swapped;
+    for (int i = 0; i < n; i++) {
+        int minIndex = i;
+        swapped = false;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+                swapped = true;
             }
         }
+        if (swapped)
+            swap(arr, minIndex, i);
     }
-    public void swap(int [] arr, int minIndex, int index){
-        int temp;
-        temp = arr[minIndex];
-        arr[minIndex] = arr[index];
-        arr[index] = temp;
-    }
+}
+public void swap(int [] arr, int minIndex, int index){
+    int temp;
+    temp = arr[minIndex];
+    arr[minIndex] = arr[index];
+    arr[index] = temp;
 }
